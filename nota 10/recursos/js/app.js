2377780e3930166c71d3dfdb735c5211dc5de88d
Vinicio Muzzi <162225896+vinicmor@users.noreply.github.com>
2024-06-05 21:32:24 -0400

    Add files via upload

---
+++ nota 10/recursos/js/app.js
@@ -0,0 +1,21 @@
+function registerlogin(){
+
+let login= localStorage.getItem("login")
+if (login ===null)
+    localStorage,setItem("login","icet2024")
+localStorage,setItem("password","icet2024")
+
+    }
+{
+    function uservalidation(){
+
+        let login= localStorage.getItem("login")
+        if (login ===null)
+            localStorage,setItem("login","icet2024")
+        localStorage,setItem("password","icet2024")
+
+
+
+
+
+}
